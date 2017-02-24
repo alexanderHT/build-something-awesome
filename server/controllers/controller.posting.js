c70559bce8a2d108c0b90eeaeaf5f68fd2e5daaa
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-24 15:09:01 +0700

    update depan

--- server/controllers/controller.posting.js
+++ server/controllers/controller.posting.js
@@ -1,17 +1,58 @@
-var modelPost = require('../models/model.posting.js');
+var modelPost = require('../models/model.posting')
 
 var controllerPosting = {
   /* get all posting */
-  getAllPosting: function(req, res){
+  getAllPosting: function (req, res) {
     modelPost
       .find({})
       .populate('postBy')
       .exec(function (err, data) {
-      if (err) return handleError(err)
-      // else console.log({data})
-      else res.json(data)
+        if (err) return handleError(err)
+        // else console.log({data})
+        else res.json(data)
       })
+  },
+  /* create one post */
+  addOnePost: function (req, res) {
+    console.log(req.body.title)
+    console.log(req.body.description)
+    console.log(req.body.img)
+    console.log(req.body.userid)
+
+    let newPost = modelPost({
+      title: req.body.title,
+      description: req.body.description,
+      img: req.body.img,
+      postBy: req.body.userid
+    })
+
+    newPost.save(function (err, data) {
+      if (err) throw err
+      modelPost
+        .findOne({ _id: data._id })
+        .populate('postBy')
+        .exec(function (err, data) {
+          if (err) return handleError(err)
+          else res.json(data)
+        })
+    })
+  },
+  /* get one post */
+  getOnePost: function (req, res) {
+    modelPost
+      .findOne({ _id: req.params.id })
+      .populate('postBy')
+      .exec(function (err, data) {
+        if (err) return handleError(err)
+        else res.json(data)
+      })
+  },
+  deleteOnePost: function (req, res) {
+    modelPost.findOneAndRemove({ _id: req.body.id }, function (err, data) {
+      if (err) throw err
+      res.json(data._id)
+    })
   }
 }
 
-module.exports = controllerPosting;
+module.exports = controllerPosting
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var controllerPosting = require('../controllers/controller.posting');
+var express = require('express')
+var router = express.Router()
+var controllerPosting = require('../controllers/controller.posting')
 
-router.post('/', controllerPosting);
-router.post('/', controllerPosting);
+/* get all posting from database */
+router.get('/', controllerPosting.getAllPosting)
+/* create one posting */
+router.post('/', controllerPosting.addOnePost)
+/* get one posting from database */
+router.get('/post/:id', controllerPosting.getOnePost)
+/* delete one posting from database */
+router.delete('/', controllerPosting.deleteOnePost)
 
-module.exports = router;
+module.exports = router
